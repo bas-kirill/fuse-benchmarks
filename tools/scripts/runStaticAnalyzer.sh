43661a6284c2ab7d7c521fd07d670b2b5a84d172
skywalker <baskirill.an@gmail.com>
2024-03-08 19:42:36 +0200

    feat(scripts): add `checkov` (static analyzer)

--- tools/scripts/runStaticAnalyzer.sh
+++ tools/scripts/runStaticAnalyzer.sh
@@ -3,5 +3,4 @@ set -e
 currentDir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 rootDir="$currentDir/../../"
 
-(cd $rootDir && exec terraform fmt -list=true -recursive)
-(cd $rootDir && exec terraform validate)
+(cd $rootDir && checkov -d ./)
