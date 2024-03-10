74e5a1d412efa28b94a674fb34134dd6885d8ef5
skywalker <baskirill.an@gmail.com>
2024-03-10 13:53:15 +0200

    chore(s3fs-benchmark): clean up debug info

--- infra/configs/s3fs.sh
+++ infra/configs/s3fs.sh
@@ -2,22 +2,14 @@
 set -e
 sudo su
 
-touch /home/ubuntu/0.txt
-
 apt update -y
 apt install s3fs -y
 apt install fio -y
 apt install awscli -y
 
-touch /home/ubuntu/1.txt
-
 mkdir ${MOUNT}
 
-touch /home/ubuntu/2.txt
-
 s3fs ${BUCKET} ${MOUNT} -o parallel_count=400,ensure_diskfree=1024,del_cache,use_cache=/tmp/,iam_role=${IAM_ROLE}
-
-touch /home/ubuntu/3.txt
 
 cat <<EOF > ${FIO_CONFIG}
 [global]
@@ -36,18 +28,10 @@ rwmixread=${RWMIXREAD}
 size=${SIZE}
 EOF
 
-touch /home/ubuntu/4.txt
-
 fio ${FIO_CONFIG} > ${RESULT}
 
-touch /home/ubuntu/5.txt
-
 aws s3 cp ${RESULT} s3://${REPORT_BUCKET}/${REPORT_NAME}
-
-touch /home/ubuntu/6.txt
 
 aws s3 rm s3://${BUCKET} --recursive
 instance_id=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 aws ec2 terminate-instances --instance-ids $instance_id --region ${AWS_REGION}
-
-touch /home/ubuntu/7.txt
